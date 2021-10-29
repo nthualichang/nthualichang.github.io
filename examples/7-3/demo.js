a07aee68cee9d1b05449ffbbe2cecb1c35acce59
nthualichang <alicec168@gmail.com>
2021-10-29 10:59:47 +0800

    s

--- examples/7-3/demo.js
+++ examples/7-3/demo.js
@@ -17,11 +17,15 @@ var emptyVar; // A statement that creates a variable called "emptyVar"  可想�
 //console.log(emptyVar); //undefined
 
 var num = 5; // A statement that creates a variable called "num" with a default numeric value
-//console.log(num)
+//console.log(num);
 
 var x = 10, y = 20, z = 40; // Create multiple variables in one single statement
 
 //var x = 20; // Potential Error
 
 x = 25; // Change the value of a variable
-console.log(x);
+//console.log(x);
+
+var name = 'Mike'; // Create a variable with a string as its default value
+var anotherName = "John"; // A pair of double-quotation marks also work for a string
+//var badQuotation = 'Ah"; // Different qutation marks cause errors
