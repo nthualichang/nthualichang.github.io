0e727fdc52f8fe26e08a29d225f98b0ed56f873f
nthualichang <alicec168@gmail.com>
2021-10-29 11:49:23 +0800

    s

--- examples/7-5/demo.js
+++ examples/7-5/demo.js
@@ -7,5 +7,6 @@ console.log(num); // No problem, you will get 1.
 // This is another code block
 {
 	let str = 'This is a string.'; // A local variable created with 'let'
+	console.log(str); // The target string.
 }
-console.log(str); // Error: str is undefined
+console.log(str); // Error: str is undefined.
