762993b9ca02a7dbc05db27a869d284b8dcc87db
nthualichang <alicec168@gmail.com>
2021-10-29 10:26:59 +0800

    s

---
+++ examples/7-3/demo.js
@@ -0,0 +1,8 @@
+//Single-line comment
+
+/* Multi-line comment:
+   When you have lots to say, you need this.
+*/
+
+// A "Statement" ends with a semi-colon
+console.log('This is an embedded script');
