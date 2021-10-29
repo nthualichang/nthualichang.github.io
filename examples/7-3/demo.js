63c48797b9a5da8410d278bd71c4dca99f64f39c
nthualichang <alicec168@gmail.com>
2021-10-29 10:29:36 +0800

    s

--- examples/7-3/demo.js
+++ examples/7-3/demo.js
@@ -4,5 +4,10 @@
    When you have lots to say, you need this.
 */
 
-// A "Statement" ends with a semi-colon
+// A "statement" ends with a semi-colon
 console.log('This is an embedded script');
+
+// A "code block" is defined with a pair of curly brackets {}
+{
+	console.log('This message is from a code block.');
+}
