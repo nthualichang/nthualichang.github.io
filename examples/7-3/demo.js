cdb609797bdabee67dccf700bff4365a9393c8c0
nthualichang <alicec168@gmail.com>
2021-10-29 10:53:47 +0800

    s

--- examples/7-3/demo.js
+++ examples/7-3/demo.js
@@ -14,7 +14,14 @@
 
 // Variable
 var emptyVar; // A statement that creates a variable called "emptyVar"  可想成“容器”
-console.log(emptyVar); //undefined
+//console.log(emptyVar); //undefined
 
 var num = 5; // A statement that creates a variable called "num" with a default numeric value
-console.log(num)
+//console.log(num)
+
+var x = 10, y = 20, z = 40; // Create multiple variables in one single statement
+
+//var x = 20; // Potential Error
+
+x = 25; // Change the value of a variable
+console.log(x);
