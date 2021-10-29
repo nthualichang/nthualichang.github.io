406a3f5848c355f5ceaef8e505be25f4b526d730
nthualichang <alicec168@gmail.com>
2021-10-29 11:23:30 +0800

    s

---
+++ examples/7-4/demo.js
@@ -0,0 +1,10 @@
+var math1 = 2 + 5; // + Addition
+var math2 = 3 * 4.5; // * Multiplying
+var math3 = 1 - 5; // - Substracting
+var math4 = 20 / 5; // / Dividing
+
+var math5 = 1 - 5 * 20; // Multiply first
+// console.log(math5);
+
+var calc = math4 + math5; // Add math4 to math5 and store the result in the variavle 'calc'
+console.log(calc);
