159ec68db49dacece1c80a5f638ad2e3c4b7a969
nthualichang <alicec168@gmail.com>
2021-10-29 10:45:24 +0800

    s

--- examples/7-3/demo.js
+++ examples/7-3/demo.js
@@ -5,9 +5,13 @@
 */
 
 // A "statement" ends with a semi-colon
-console.log('This is an embedded script');
+//console.log('This is an embedded script');
 
 // A "code block" is defined with a pair of curly brackets {}
 {
-	console.log('This message is from a code block.');
+	//console.log('This message is from a code block.');
 }
+
+// Variable
+var emptyVar: // A statement that creates a variable called "emptyVar"  可想成“容器”
+console.log(emptyVar); //undefined
