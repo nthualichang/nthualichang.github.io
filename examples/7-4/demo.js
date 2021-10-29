a5728732de462b3f35b11e9c36766d07a35ab332
nthualichang <alicec168@gmail.com>
2021-10-29 11:35:31 +0800

    s

--- examples/7-4/demo.js
+++ examples/7-4/demo.js
@@ -13,8 +13,11 @@ var msg = 'The result of calculationo is: ' + calc; // Combine a numeric value w
 //console.log(msg); 
 
 var notANumber = '0' + '100'; // These are strings not numbers!
-//console.log(notANumber);
+console.log(notANumber);
 
+// Mixing math calculations with string combination
 var test1 = 'Result:' + 5 + 10;
 var test2 = 'Result:' + 5 * 10;
+console.log(test1);
+console.log(test2);
 
