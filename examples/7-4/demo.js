e5d5d5982e6017f11cd5a36f604d8d73537f3644
nthualichang <alicec168@gmail.com>
2021-10-29 11:26:44 +0800

    s

--- examples/7-4/demo.js
+++ examples/7-4/demo.js
@@ -7,4 +7,7 @@ var math5 = 1 - 5 * 20; // Multiply first
 // console.log(math5);
 
 var calc = math4 + math5; // Add math4 to math5 and store the result in the variavle 'calc'
-console.log(calc);
+//console.log(calc);
+
+var msg = 'The result of calculationo is: ' + calc; // Combine a numeric value with a string as a string
+console.log(msg)
