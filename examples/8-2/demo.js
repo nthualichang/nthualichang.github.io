20be548a175a142401cb05107af555c3958dda49
nthualichang <alicec168@gmail.com>
2021-11-05 11:21:09 +0800

    s

--- examples/8-2/demo.js
+++ examples/8-2/demo.js
@@ -1,16 +1,33 @@
 var numArray = [1, 5, 2, 11, 7];
 
-console.log(numArray[0]); // First position
-console.log(numArray[1]); // Second position
+//console.log(numArray[0]); // First position
+//console.log(numArray[1]); // Second position
 
-console.log(numArray[4]); // Last position
+//console.log(numArray[4]); // Last position
 numArray[4] = 18;
-console.log(numArray[4]); //Last position with a different value
+//console.log(numArray[4]); //Last position with a different value
 
-console.log(numArray); // Entire array
+//console.log(numArray); // Entire array
 
 var students = ['Tom', 'Mary', 'Victor', 'John'];
 
-var proflie = ['TY Chen', 'NTHU', 40, 180, 72];
+// Mixed types of data in an array; not recommended
+//var proflie = ['TY Chen', 'NTHU', 40, 180, 72];
 
-console.log(proflie[0]);
+//console.log(proflie[0]);
+
+//var name = 'TYChen', affiliation = 'NTHU', age = 40;
+
+var emptyObj = {}; // An empty object
+
+// A good progile object
+var profile = {
+	name: 'TYChen',
+	age: 39,
+	affiliation: 'NTHU',
+	height: 180,
+	weight:72
+};
+
+console.log(progile.name); // Get the value of the "name" key
+console.log(profile['name']); // Same
