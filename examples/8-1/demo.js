07d2bcfbf7ee398584e82c016bf7fd156d22e1f6
nthualichang <alicec168@gmail.com>
2021-11-05 09:56:30 +0800

    s

--- examples/8-1/demo.js
+++ examples/8-1/demo.js
@@ -1,7 +1,15 @@
 function printMsg(msg) {
-	Let finalMsg = 'Message: '+ msg;
+	let finalMsg = 'Message: '+ msg;
 	console.log(finalMsg);
 }
 
 printMsg('Test!');
 printMsg('Another Test!');
+
+function userMsg(name, msg) {
+	let finalMsg = name + 'says: ' + msg;
+	console.log(finalMsg);
+}
+
+userMsg('Victor', 'Surprise!');
+userMsg('Mary', 'I love Victor!');
