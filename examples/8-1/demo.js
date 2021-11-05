5e75dfb13dddba8d88dd562142e53e4be7d4ca91
nthualichang <alicec168@gmail.com>
2021-11-05 10:07:13 +0800

    s

--- examples/8-1/demo.js
+++ examples/8-1/demo.js
@@ -3,13 +3,27 @@ function printMsg(msg) {
 	console.log(finalMsg);
 }
 
-printMsg('Test!');
-printMsg('Another Test!');
+//printMsg('Test!');
+//printMsg('Another Test!');
 
 function userMsg(name, msg) {
-	let finalMsg = name + 'says: ' + msg;
+	let finalMsg = name + ' says: ' + msg;
 	console.log(finalMsg);
 }
 
-userMsg('Victor', 'Surprise!');
-userMsg('Mary', 'I love Victor!');
+//userMsg('Victor', 'Surprise!');
+//userMsg('Mary', 'I love Victor!');
+
+function complexCalc(x, y) {
+	let result = 0;
+	result = x * y;
+	result = result / (x - y);
+	result = result * y - x;
+	alert('Hey!')
+	return result;
+	alert('Not executed QQ.');
+
+}
+
+var calcResult = complexCalc(4,6);
+console.log(calcResult);
