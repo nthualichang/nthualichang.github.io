38d0aeb54a79f2529e76b5b0c044857fcbaa1b25
nthualichang <alicec168@gmail.com>
2021-11-12 09:54:33 +0800

    s

--- examples/9-1/demo.js
+++ examples/9-1/demo.js
@@ -8,5 +8,13 @@ var winHeight = window.innerHeight;
 //console.log(winWidth);
 //console.log(winHeight);  //這兩行沒印出來
 
-window.location.href = 'https://google.com';
+//window.location.href = 'https://google.com';
 //window.location.pathname = '/nosuchpath';
+
+window.onload = function() {   // 確定整個html建立好再存取html的內容
+	//alert('Website is ready!');
+	// window.document: The entire HTML document
+	// document: The entire HTML document
+	let siteTitle = document.title;
+	console.log(siteTitle);
+}
