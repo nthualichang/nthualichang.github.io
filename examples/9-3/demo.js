14216ca470184aa070d0b263ac882a0f3be4040a
nthualichang <alicec168@gmail.com>
2021-11-12 12:02:18 +0800

    s

--- examples/9-3/demo.js
+++ examples/9-3/demo.js
@@ -1,6 +1,7 @@
 function anotherMsg() {
 	alert('I say the mouse cursor has entered the first div!');
 }
+
 window.onload = function() {
 	let bodyEM = document.querySelector('body');
 	bodyEM.addEventListener('click', function() {
@@ -11,9 +12,15 @@ window.onload = function() {
 	let firstDiv = document.querySelector('div');
 	//firstDiv.addEventListener('mouseenter', anotherMsg);
 
-	let button = document.querySelector('button');
-	button.addEventListener('click',function(){
+	//let button = document.querySelector('button');
+	let changeButton = document.getElementById('changeDiv1');
+	changeButton.addEventListener('click',function(){
 		firstDiv.style.backgroundColor = 'red';
 	})
+
+	let changeResetButton = document.getElementById('changeResetDiv1');
+	changeResetButton.addEventListener('click', function() {
+		firstDiv.classList.toggle('gold');
+	});
 };
 
