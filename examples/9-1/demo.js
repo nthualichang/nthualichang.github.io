ab555720e0e9a9259bd0dea3b431cef0fa3ecdea
nthualichang <alicec168@gmail.com>
2021-11-12 09:38:22 +0800

    s

--- examples/9-1/demo.js
+++ examples/9-1/demo.js
@@ -5,5 +5,8 @@
 var winWidth = window.innerwidth;
 var winHeight = window.innerHeight;
 
-console.log(winWidth);
-console.log(winHeight);
+//console.log(winWidth);
+//console.log(winHeight);  //這兩行沒印出來
+
+window.location.href = 'https://google.com';
+//window.location.pathname = '/nosuchpath';
