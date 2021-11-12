6edb11b6dc43a6618bf8d0391aa7ddd112546dd9
nthualichang <alicec168@gmail.com>
2021-11-12 11:16:48 +0800

    s

--- examples/9-2/demo.js
+++ examples/9-2/demo.js
@@ -6,4 +6,11 @@ window.onload = function() {
 
 	let allDivPs = document.querySelectorAll('div > p');
 	allDivPs[1].style.cssText = 'background-color: green; color: red;';
+
+	let allDivs = document.getElementsByTagName('div');
+	//allDivs[4].style.cssText = 'background-color: gold; color: white;';
+	//allDivs[5].style.cssText = 'background-color: gold; color: white;';
+	//console.log(allDivs[4].classList);
+	allDivs[4].classList.add('gold');
+	allDivs[5].classList.add('gold');
 }
