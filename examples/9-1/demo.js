19ec6eff546d51aaed73958c8332195ae9a3211d
nthualichang <alicec168@gmail.com>
2021-11-12 09:35:34 +0800

    s

--- examples/9-1/demo.js
+++ examples/9-1/demo.js
@@ -1,4 +1,9 @@
 //console.log('Hi!');
 
 // window: The entire browser window
- 
+
+var winWidth = window.innerwidth;
+var winHeight = window.innerHeight;
+
+console.log(winWidth);
+console.log(winHeight);
