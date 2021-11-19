6f5a3faae0fa7e66e716e1e2df2a1508b4f74f59
nthualichang <alicec168@gmail.com>
2021-11-19 10:35:05 +0800

    s

--- examples/10-1/demo.js
+++ examples/10-1/demo.js
@@ -1,9 +1,31 @@
 window.onload = function () {
 	if(false) {
-		alert('true!');
+		//alert('true!');
 	}
 	else {
-		alert('false!');
-
+		//alert('false!');
 	}
-}
+
+	// Real conditions
+	if(1 === 2) {
+		//alert('true!');
+	}
+	/*else { // 'else' is optional
+		alert('false!');
+	}*/
+
+	// !== means 'not equivalent to'
+	if(1 !== 2) {
+		//alert('true!')
+	}
+
+	if(2 === 2) {
+		alert('Condition 1 is true!');
+	}
+	else if('a' === 'b') {
+		alert('Condition 2 is true!');
+	}
+	else {
+		alert('Neither is true QQ.')
+	}
+};
