53edcafb3a02cb05a4c43478155b173d60772f69
nthualichang <alicec168@gmail.com>
2021-11-19 11:24:42 +0800

    s

--- examples/10-1/demo.js
+++ examples/10-1/demo.js
@@ -1,3 +1,17 @@
+function changeDivCSS() {
+	let buttonId = this.id;
+	let divs = document.getElementsByTagName('div');
+	if(buttonId === 'changeDiv1') {
+		divs[0].style.cssText = 'background-color: red; width: 100px; height: 40px;';
+	}
+	else if(buttonId === 'changeDiv2') {
+		divs[1].style.cssText = 'background-color: black; width: 60px; height: 150px;';
+	}
+	else {
+		divs[2].style.cssText = 'background-color: gold; width: 200px; height:200px;';
+	}
+};
+
 window.onload = function () {
 	if(false) {
 		//alert('true!');
@@ -32,12 +46,16 @@ window.onload = function () {
 
 	// AND vs. OR in the same line
 	if(2 === 2 && 15 === 10) { // && means AND
-		alert('Condition 1 is true.');
+		//alert('Condition 1 is true.');
 	}
 	else if(2 !== 2 || 15 === 10) { // || means OR
-		alert('Condition 2 is true.');
+		//alert('Condition 2 is true.');
 	}
 	else {
-		alert('Neither is true.');
+		//alert('Neither is true.');
 	}
+
+	document.getElementById('changeDiv1').addEventListener('click', changeDivCSS);
+	document.getElementById('changeDiv2').addEventListener('click', changeDivCSS);
+	document.getElementById('changeDiv3').addEventListener('click', changeDivCSS);
 };
