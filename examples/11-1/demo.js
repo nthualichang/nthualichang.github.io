e1829c1261d94568a69ef154a780e2ea7a586dba
nthualichang <alicec168@gmail.com>
2021-11-26 09:48:06 +0800

    s

--- examples/11-1/demo.js
+++ examples/11-1/demo.js
@@ -1,6 +1,8 @@
-windows.onload = function() {
+window.onload = function() {
 	let button = document.querySelector('button');
-	button.addEventListener('click', function() {
+	// the 'event' information is passed over by the browser
+	button.addEventListener('click', function(event) {
+		event.stopPropagation(); // In the 'event' object, there's a stopPropagation function.
 		alert('button clicked!');
 	});
 	let div = document.querySelector('div');
