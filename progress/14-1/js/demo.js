1b83d3f27b798a9c9b01a8556fd4a3f1af014e7d
nthualichang <alicec168@gmail.com>
2021-12-17 10:10:04 +0800

    s

---
+++ progress/14-1/js/demo.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+	$('nav > a:first-of-type').on('click', function(event) {
+		event.preventDefault(); // stop the navigating default action of a hyperlink.
+		// Take all the non-first <a> elements under <nav> and apply the toggle() function.
+		$('nav > a:not(:first-of-type').toggle(600);
+	});
+};
